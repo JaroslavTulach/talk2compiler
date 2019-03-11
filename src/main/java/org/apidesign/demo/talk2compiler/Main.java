82713014e621caeae0d2577141e066c0c84874cb
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2019-03-11 09:15:59 +0100

    Build and compile a cache of known values

--- src/main/java/org/apidesign/demo/talk2compiler/Main.java
+++ src/main/java/org/apidesign/demo/talk2compiler/Main.java
@@ -1,17 +1,30 @@
 package org.apidesign.demo.talk2compiler;
 
 import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.nodes.RootNode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main extends RootNode {
     static final Main MAIN = new Main();
     static final CallTarget CODE = Truffle.getRuntime().createCallTarget(MAIN);
 
+    private final Map<String, Integer> ids = new HashMap<>();
+
+    @CompilerDirectives.CompilationFinal
+    private Chain cache;
+
     private Main() {
         super(null);
+    }
+
+    public void addMapping(String text, int id) {
+        ids.put(text, id);
     }
 
     public static void main(String... args) {
@@ -21,12 +34,43 @@ public class Main extends RootNode {
 
     @Override
     public Object execute(VirtualFrame frame) {
-        final String name = (String) frame.getArguments()[0];
-        return formatGreeting("Hello from %s!", name);
+        final String value = (String) frame.getArguments()[0];
+        return findInChainAndThenId(value);
+    }
+
+    @ExplodeLoop
+    private Integer findInChainAndThenId(String key) {
+        Chain item = cache;
+        while (item != null) {
+            if (item.key == key) {
+                return item.value;
+            }
+            item = item.next;
+        }
+        Integer value = findId(key);
+        if (cache == null || cache.count < 2) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            cache = new Chain(key, value, cache);
+        }
+        return value;
     }
 
     @TruffleBoundary
-    private static String formatGreeting(String msg, String name) {
-        return String.format(msg, name);
+    private Integer findId(String name) {
+        return ids.get(name);
+    }
+
+    private static final class Chain {
+        public final String key;
+        public final Integer value;
+        public final Chain next;
+        public final int count;
+
+        Chain(String key, Integer value, Chain next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+            this.count = next == null ? 1 : next.count + 1;
+        }
     }
 }
--- src/test/java/org/apidesign/demo/talk2compiler/MainTest.java
+++ src/test/java/org/apidesign/demo/talk2compiler/MainTest.java
@@ -1,30 +1,37 @@
 package org.apidesign.demo.talk2compiler;
 
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
-    @Before
-    public void warmingUp() {
+    @After
+    public void warmingUp() throws Exception {
         int count;
         if (Boolean.getBoolean("noigv")) {
             // Skip warmup if IGV dump isn't requested
             count = 1;
         } else {
-            count = 1000000;
+            count = 10000000;
         }
-        for (int i = 0; i < count; i++) {
-            sayHelloTruffle();
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < count; i++) {
+                Main.CODE.call("kuk");
+            }
+            Thread.sleep(1000);
         }
     }
 
     @Test
     public void checkSayHello() {
-        Assert.assertEquals("Hello from Truffle!", sayHelloTruffle());
-    }
-
-    private static Object sayHelloTruffle() {
-        return Main.CODE.call("Truffle");
+        Main.MAIN.addMapping("huh", 33);
+        Main.MAIN.addMapping("kuk", 66);
+        Main.CODE.call("kuk");
+        Main.CODE.call("buk");
+        Main.CODE.call("muk");
+        Main.CODE.call("puk");
+        Assert.assertEquals(66, Main.CODE.call("kuk"));
+        Assert.assertNull(Main.CODE.call("buk"));
+        Assert.assertEquals(33, Main.CODE.call("huh"));
     }
 }
