7639eac13306d49947faf7bf7108b6634a617c22
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2021-03-29 14:47:05 +0200

    Updating to newest master

--- src/main/java/org/apidesign/demo/talk2compiler/Main.java
+++ src/main/java/org/apidesign/demo/talk2compiler/Main.java
@@ -1,45 +1,34 @@
 package org.apidesign.demo.talk2compiler;
 
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.RootNode;
 
 public class Main extends RootNode {
-    static final Main MAIN = new Main();
-    static final CallTarget CODE = Truffle.getRuntime().createCallTarget(MAIN);
+    private final int mul;
+    final CallTarget CODE = Truffle.getRuntime().createCallTarget(this);
 
-    private Main() {
+    public Main(int mul) {
         super(null);
+        this.mul = mul;
+    }
+
+    public static void main(String... args) {
     }
 
     @Override
     public Object execute(VirtualFrame frame) {
-        final String name = (String) frame.getArguments()[0];
-        return formatGreeting("Hello from %s!", name);
+        int[] arr = (int[]) frame.getArguments()[0];
+        return mulAndSum(arr);
     }
 
-    @TruffleBoundary
-    private static String formatGreeting(String msg, String name) {
-        return String.format(msg, name);
-    }
-
-    public static void main(String... args) {
-        String who = args.length > 0 ? args[0] : "unknown";
-        int cnt;
-        if (Boolean.getBoolean("noigv")) {
-            cnt = 1;
-        } else {
-            cnt = args.length > 1 ? Integer.parseInt(args[1]) : 10000000;
+    Object mulAndSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int value = arr[i];
+            sum += mul * value;
         }
-        int print = 1;
-        for (int i = 1; i <= cnt; i++) {
-            final Object result = CODE.call(who);
-            if (i >= print) {
-                System.err.println("run #" + i + " result: " + result);
-                print *= 2;
-            }
-        }
+        return sum;
     }
 }
--- src/test/java/org/apidesign/demo/talk2compiler/MainTest.java
+++ src/test/java/org/apidesign/demo/talk2compiler/MainTest.java
@@ -1,30 +1,75 @@
 package org.apidesign.demo.talk2compiler;
 
-import org.junit.Assert;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
-    @Before
-    public void warmingUp() {
-        int count;
-        if (Boolean.getBoolean("noigv")) {
-            // Skip warmup if IGV dump isn't requested
-            count = 1;
-        } else {
-            count = 10000000;
+    private static final Main MUL_BY_3 = new Main(3);
+
+    private class NR implements Iterator<Integer> {
+        private final Random generator;
+        int remaining;
+
+        NR(long seed, int remaining) {
+            this.generator = new Random(seed);
+            this.remaining = remaining;
         }
-        for (int i = 0; i < count; i++) {
-            sayHelloTruffle();
+
+        @Override
+        public boolean hasNext() {
+            return remaining > 0;
+        }
+
+        @Override
+        public Integer next() {
+            if (remaining == 0) {
+                throw new NoSuchElementException();
+            }
+            remaining--;
+            return generator.nextInt();
+        }
+
+        public int[] toArray() {
+            int[] res = new int[remaining];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = next();
+            }
+            return res;
+        }
+    }
+
+    @Before
+    public void warmUp() {
+        for (int i = 0; i < 1000000; i++) {
+            long seed = System.currentTimeMillis();
+            Object r1 = MUL_BY_3.CODE.call(new NR(seed, 1000).toArray());
+            assertEquals(Integer.class, r1.getClass());
+            Object r2 = MUL_BY_3.mulAndSum(new NR(seed, 1000).toArray());
+            assertEquals(r1, r2);
         }
     }
 
     @Test
-    public void checkSayHello() {
-        Assert.assertEquals("Hello from Truffle!", sayHelloTruffle());
-    }
+    public void saySumItAll() {
+        final int[] arr = new NR(System.currentTimeMillis(), 1000000).toArray();
+        long before = System.currentTimeMillis();
+        int fast = -1;
+        for (int i = 0; i < 100; i++) {
+            fast = (int) MUL_BY_3.CODE.call(arr);
+        }
+        int slow = -1;
+        long middle = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            slow = (int) MUL_BY_3.mulAndSum(arr);
+        }
+        long then = System.currentTimeMillis();
+        assertEquals(slow, fast);
 
-    private static Object sayHelloTruffle() {
-        return Main.CODE.call("Truffle");
+        fail(String.format("Fast %d ms, slow %d ms", middle - before, then - middle));
     }
 }
